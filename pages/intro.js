0b4dbe917bb5480e7175adde699f1c78781ce216
seohyun9672 <ajeong4@my.bcit.ca>
2022-04-13 06:16:39 -0700

    update intro layout

--- pages/intro.js
+++ pages/intro.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import CateGrid from '../comps/CateGrid'
-import Layout from "../comps/Layout/page";
-import { PageTitle } from '../data/intro_content';
+import { PageTitle, BtnText } from '../data/intro_content';
+import Menu from '../comps/Menu'
+import { useRouter } from 'next/router';
 
 const Em = styled.span`
     color: #FF9042;
@@ -15,15 +16,32 @@ const LayoutComp = styled.div`
     justify-content: center;
     align-items: center;
     margin-top: 6.5em;
+
+    @media only screen and (max-width: 600px) and (max-height: 750px) {
+        margin-top: 5em;
 `
 
 export default function Intro() {
+
+    const r = useRouter();
     return (
         <LayoutComp>
-            <Layout></Layout>
+            <Menu />
             <h1>{PageTitle}</h1>
             <h4>Calculate how many Earth minutes you saved today based our <Em>12-question quiz</Em> on the following categories:</h4>
             <CateGrid />
+            <button
+                className='primary large'
+                onClick={
+                    () => {
+                        r.push(
+                            {
+                                pathname: "/questions"
+                            }
+                        )
+                    }
+                }
+            >{BtnText}</button>
         </LayoutComp>
     )
 }
