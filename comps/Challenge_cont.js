ce1b83aac8fd815df5c4572ac405d606215bcd6d
seohyun9672 <ajeong4@my.bcit.ca>
2022-04-13 23:13:49 -0700

    edit challenges

--- comps/Challenge_cont.js
+++ comps/Challenge_cont.js
@@ -1,32 +1,48 @@
 import { useRouter } from "next/router";
 import styled from 'styled-components';
 import { cat1, cat2, cat3, cat4, cat5, cat6 } from "../data/intro_content";
-import { SecondaryButton } from "./Button";
-
 
 const Challenge_cont = styled.div`
-border:1px solid lightgrey;
-border-radius:8px;
-display:flex;
-flex-direction:column;
-align-items:flex-start;
-padding:15px;
-margin:1em;
+    border: 1px solid lightgrey;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    padding-top: 0;
+    text-align: left;
+    margin-bottom: 1em;
 `
 
- 
-export function ChallengeHeading() {
-    return <div class="challenge-heading">
-       <h2 class="challenge-h2">Challenge Category</h2>
-        <img class="challenge-img" src={cat1.img} />
-    </div>
-}
+// export function ChallengeHeading() {
+//     return <div class="challenge-heading">
+//        <h2 class="challenge-h2">Challenge Category</h2>
+//         <img class="challenge-img" src={cat1.img} />
+//     </div>
+// }
+
+const ChallengeHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+`
+const Button = styled.button`
+    margin-left: auto;
+    margin-top: 1em
+`
+const ChallengeImg = styled.img`
+    width: 20%;
+    height: 80%;
+`
 
 export default function ChallengeCont() {
     return <Challenge_cont>
-        <ChallengeHeading />
+        <ChallengeHeader>
+            <h2>Challenge Category</h2>
+            <ChallengeImg src={cat1.img} />
+        </ChallengeHeader>
         <h3>Take the bus twice a week instead of driving to work!</h3>
-        <p>Earth minutes saved in a year: 120 mins</p>
-        <SecondaryButton></SecondaryButton>
+        <div>Earth minutes saved in a year: 120 mins</div>
+        <Button className="secondary small">Start</Button>
     </Challenge_cont>
 }
